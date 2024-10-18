de481e9cbe895223856222256dcb68e6c74ddeca
JjonakRyu <jjonak.ryu25@gmail.com>
2024-10-18 10:11:48 +0200

    progress

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private Transform m_Target;
+
+    private NavMeshAgent m_agent;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        m_agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_agent.SetDestination(m_Target.position);
+    }
+}
